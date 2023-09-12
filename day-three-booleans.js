c58126df27feba5ce56402358c1fc58e987d5019
aytaa <Odak1098!&>
2023-09-12 23:47:56 +0300

    day three

---
+++ day-three-booleans.js
@@ -0,0 +1,14 @@
+// Booleans sadece iki değer döndürür true ve false aşağıdaki örnekleri console.log yardımı ile deneyimleyebilirsiniz...
+
+let isLightOn = true
+let isRaining = false
+let isHungry = false
+let isMarried = true
+let truValue = 4 > 3    // true
+let falseValue = 4 < 3  // false
+
+// isMarried yazdırmak istediğimde true veya false değerlerinden birini döndürür
+console.log(isMarried)
+
+// Eğer işlemin sonucu truValue ya eşit ise true döndürür değilse false (true)
+console.log(truValue)
---
+++ day-two-variables.js
@@ -0,0 +1,24 @@
+//  Farklı veri türlerinin farklı değişkenlerini göstermek
+let firstName = 'Aytac'
+let lastName = 'Unal'
+let country = 'Turkey'
+let isMarried = true
+
+// Değişkenleri sayı değerleriyle göstermek
+const gravity = 9.81
+const boilingPoint = 80 // water boiling point, temperature in oC
+const PI = 3.14 // geometrical constant
+
+// Değişkenler virgülle ayrılmış olarak tek satırda da göstermek
+let name = 'Aytac', //name of a person
+    job = 'developer',
+    live = 'Samsun'
+
+
+//Bu örnekleri console.log kullanarak çalıştırabilirsiniz...
+console.log(job)
+console.log(live)
+console.log(name)
+console.log(PI)
+console.log(boilingPoint)
+console.log(gravity)
